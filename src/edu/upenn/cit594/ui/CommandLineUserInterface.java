b71daed1efb5b1ad9f656f3962f6a8a5cc00e32c
cdhenry <cdhenry@gmail.com>
2020-04-17 09:00:10 -0500

    feat: added cli constructor and start class

--- src/edu/upenn/cit594/ui/CommandLineUserInterface.java
+++ src/edu/upenn/cit594/ui/CommandLineUserInterface.java
@@ -1,4 +1,39 @@
 package edu.upenn.cit594.ui;
 
+import edu.upenn.cit594.processor.ParkingViolationProcessor;
+import edu.upenn.cit594.processor.PopulationProcessor;
+import edu.upenn.cit594.processor.PropertyValueProcessor;
+
+/**
+ * A command line interface class for outputting text via the console to the user
+ *
+ * @author Chris Henry + Tim Chung
+ */
 public class CommandLineUserInterface {
+    protected ParkingViolationProcessor parkingViolationProcessor;
+    protected PopulationProcessor populationProcessor;
+    protected PropertyValueProcessor propertyValueProcessor;
+
+    /**
+     * Constructs a CommandLineUserInterface class for interaction with the user.
+     * Uses processor classes to deliver the desired information to the user.
+     *
+     * @param parkingViolationProcessor a parking violation processor
+     * @param populationProcessor a population processor
+     * @param propertyValueProcessor a property value processor
+     */
+    public CommandLineUserInterface(ParkingViolationProcessor parkingViolationProcessor,
+                                    PopulationProcessor populationProcessor,
+                                    PropertyValueProcessor propertyValueProcessor) {
+        this.parkingViolationProcessor = parkingViolationProcessor;
+        this.populationProcessor = populationProcessor;
+        this.propertyValueProcessor = propertyValueProcessor;
+    }
+
+    /**
+     * Starts the UI sequence
+     */
+    public void start() {
+
+    }
 }
