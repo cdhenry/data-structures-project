bbc426b17edcf8e4a3d3d459f94566c1700f5975
cdhenry <cdhenry@gmail.com>
2020-04-17 10:30:25 -0500

    feat: added scaffolding for processors and comparators

--- src/edu/upenn/cit594/processor/ParkingViolationProcessor.java
+++ src/edu/upenn/cit594/processor/ParkingViolationProcessor.java
@@ -1,4 +1,36 @@
 package edu.upenn.cit594.processor;
 
+import edu.upenn.cit594.data.ParkingViolation;
+import edu.upenn.cit594.datamanagement.ParkingViolationReader;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Stores a list of ParkingViolation objects and contains methods for performing various operations on the list
+ *
+ * @author Chris Henry + Tim Chung
+ */
 public class ParkingViolationProcessor {
+    ParkingViolationReader parkingViolationReader;
+    List<ParkingViolation> parkingViolations;
+
+    /**
+     * Constructs a ParkingViolationProcessor to store a set of ParkingViolation objects created by the
+     * ParkingViolationReader class
+     *
+     * @param parkingViolationReader
+     */
+    public ParkingViolationProcessor(ParkingViolationReader parkingViolationReader) {
+        this.parkingViolationReader = parkingViolationReader;
+        this.parkingViolations = parkingViolationReader.getAllParkingViolations();
+    }
+
+    /**
+     *
+     * @return total fine per capita
+     */
+    private Map<String, Double> getTotalFinesPerCapita(){
+        return null;
+    }
 }
